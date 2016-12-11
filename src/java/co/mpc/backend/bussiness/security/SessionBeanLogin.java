6a5fdf4c52832afb0af100bc2ca4086ca3f63584
Andres <Andres@DESKTOP-JEVVDCH>
2016-12-11 12:54:41 -0500

    dqwdqwd

--- src/java/co/mpc/backend/bussiness/security/SessionBeanLogin.java
+++ src/java/co/mpc/backend/bussiness/security/SessionBeanLogin.java
@@ -5,10 +5,13 @@
  */
 package co.mpc.backend.bussiness.security;
 
+import co.mpc.backend.model.persistence.entities.Permiso;
 import co.mpc.backend.model.persistence.entities.Usuario;
+import co.mpc.backend.model.persistence.facades.PermisoFacadeLocal;
 import co.mpc.backend.model.persistence.facades.UsuarioFacadeLocal;
 import java.io.IOException;
 import java.io.Serializable;
+import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.enterprise.context.SessionScoped;
@@ -24,6 +27,7 @@ public class SessionBeanLogin implements Serializable{
 
     private Usuario usuario;
     @EJB private UsuarioFacadeLocal ufl;
+    @EJB private PermisoFacadeLocal pfl;
     
     @PostConstruct
     public void init(){
@@ -36,6 +40,18 @@ public class SessionBeanLogin implements Serializable{
     
     public void setUsuario(Usuario usuario) {
         this.usuario = usuario;
+    }
+    
+    public List<Permiso> listaPermisos(){
+        List<Permiso> p = pfl.findAll();
+        
+        for (Permiso a: p){
+            if(a.getIdPermiso() == 1){
+            
+            }
+        }
+        
+        return pfl.findAll();
     }
 
     public void cerrarSession() throws IOException{
